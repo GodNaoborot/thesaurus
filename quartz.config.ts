4054f3c36521bdafa17273ba86c96d530e9df75d
Mikhail_Rubanov <m.rubanov@g.nsu.ru>
2026-05-22 20:03:40 +0200

    Add GitHub Pages deploy workflow, set baseUrl and ru locale

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Тезаурус: Анализ данных",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "ru-RU",
     baseUrl: "godnaoborot.github.io/thesaurus",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
